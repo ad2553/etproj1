5c587140add20179cfd65bb11b9761e73c382f78
ad2553 <ad2553@bs.amalnet.k12.il>
2024-02-19 15:29:37 +0200

    layout finished

--- app/src/main/java/com/example/etproj1/MainActivity.java
+++ app/src/main/java/com/example/etproj1/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.etproj1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Random;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
